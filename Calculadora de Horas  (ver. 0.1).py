07dbda387495e14cac99d1d1f2947fbdec25c92c
Leonardo Gomes <90984113+leogomesof@users.noreply.github.com>
2022-06-08 20:01:48 -0300

    Update Calculadora de Horas  (ver. 0.1).py

--- Calculadora de Horas  (ver. 0.1).py
+++ Calculadora de Horas  (ver. 0.1).py
@@ -1,32 +1,40 @@
+#fazer uma função para voltar no menu/ fazer uma nova operação ou não...
 a=1
-print("*** calculadora de horas ver.0.1 (alpha)***\n")
-opcao=int(input("Digite a opção:\n1 - Converter horas para minutos\n2 - Converter horas para segundos\n"
+b=0
+while(b>=0):
+    print("*** calculadora de horas ver.0.2 (alpha)***\n")
+    opcao=int(input("Digite a opção:\n1 - Converter horas para minutos\n2 - Converter horas para segundos\n"
     "3 - Converter minutos para horas\n4 - Converter minutos para segundos\nDigite 0 para fechar:\n"))
 if(opcao==0):
     a=2
-while a==1:
- if(opcao==1):
-    hora=int(input("\nConvertendo Hora para Minuto:\nDigite o valor para hora: "))
-    resultado=hora*60
-    if(resultado==1):
-        print("\n*** Resultado ***\n{} minutos é {} hora\n".format(hora1, resultado))
-    else:
-        print("\n{} horas são {} minutos.\n".format(hora, resultado))
- elif(opcao==2):
-    hora=int(input("\nDigite o valor para hora: "))
-    resultado=hora*3600
-    print("\n{} horas são {} segundos\n".format(hora,resultado))
- elif(opcao==3):
-    minutos=int(input("\nConvertendo Minutos para Horas:\nDigite o valor dos minutos: "))
-    resultado=round(minutos/60 , 2)
-    if(resultado==1):
-        print("\n*** Resultado ***\n{} minutos é {} hora\n".format(minutos, resultado))
-    else:
-        print("\n*** Resultado ***\n{} minutos são {} horas\n".format(minutos,resultado))
- elif(opcao==4):
-    minutos=int(input("\nConvertendo Minutos para Segundo:\nDigite o valor dos Minutos: "))
-    resultado=minutos*60
-    if(resultado==1):
-        print("\n*** Resultado ***\n{} minutos é {} segundo\n".format(minutos, resultado))
-    else:
-        print("\n*** Resultado ***\n{} minutos são {} segundos\n".format(minutos,resultado))
+else:
+    while a==1:
+        if(opcao==1):
+            hora=int(input("\n*** Convertendo Hora para Minuto***\n   Digite o valor para hora: "))
+            resultado=hora*60
+            if(resultado==1) or (hora==1):
+             print("\n*** Resultado ***\n{} hora é {} minutos\n".format(hora, resultado))
+            else:
+             print("\n{} horas são {} minutos.\n".format(hora, resultado))
+            b=b+1 
+        elif(opcao==2) or (hora==1):
+            hora=int(input("\nDigite o valor para hora: "))
+            resultado=hora*3600
+            if(resultado==1):
+                print("\n*** Resultado ***\n{} minutos é {} hora\n".format(hora1, resultado))
+            else:
+                print("\n{} horas são {} segundos\n".format(hora,resultado))
+        elif(opcao==3):
+            minutos=int(input("\nConvertendo Minutos para Horas:\nDigite o valor dos minutos: "))
+            resultado=round(minutos/60 , 2)
+            if(resultado==1):
+                print("\n*** Resultado ***\n{} minutos é {} hora\n".format(minutos, resultado))
+            else:
+                print("\n*** Resultado ***\n{} minutos são {} horas\n".format(minutos,resultado))
+        elif(opcao==4):
+            minutos=int(input("\nConvertendo Minutos para Segundo:\nDigite o valor dos Minutos: "))
+            resultado=minutos*60
+            if(resultado==1):
+                print("\n*** Resultado ***\n{} minutos é {} segundo\n".format(minutos, resultado))
+            else:
+                print("\n*** Resultado ***\n{} minutos são {} segundos\n".format(minutos,resultado))
