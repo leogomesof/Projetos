cf260201263b85e990440350bd3a2ac9f3a30884
Leonardo Gomes <90984113+leogomesof@users.noreply.github.com>
2022-06-08 18:05:43 -0300

    Update Calculadora de Horas  (ver. 0.1).py

--- Calculadora de Horas  (ver. 0.1).py
+++ Calculadora de Horas  (ver. 0.1).py
@@ -1,15 +1,17 @@
 a=1
-print("*** calculadora de horas ***\n")
+print("*** calculadora de horas ver.0.1 (alpha)***\n")
 while a==1:
  opcao1=int(input("Digite a opção:\n1 - Converter horas para minutos\n2 - Converter horas para segundos\n"
-                  "3 - Converter minutos para horas\n4 - Converter minutos para segundos\n"))
+    "3 - Converter minutos para horas\n4 - Converter minutos para segundos\n"))
  if(opcao1==1):
     hora=int(input("\nDigite o valor para hora: "))
     resultado=hora*60
     print("\n{} horas são {} minutos.\n".format(hora, resultado))
  elif(opcao1==2):
+  hora=int(input("\nDigite o valor para hora: "))
+  resultado=hora*3600
+  print("\n{} horas são {} segundos\n".format(hora,resultado))
+ elif(opcao1==3):
         hora=int(input("\nDigite o valor para hora: "))
         resultado=hora*3600
         print("\n{} horas são {} segundos\n".format(hora,resultado))
- elif(opcao1==3):
-    print("em fase de teste")
