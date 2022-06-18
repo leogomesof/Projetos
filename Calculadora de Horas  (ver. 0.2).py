26129e4599f28a683ad957e2a0f14b19c1c7aef1
Leonardo Gomes <90984113+leogomesof@users.noreply.github.com>
2022-06-18 02:24:53 -0300

    Update Calculadora de Horas  (ver. 0.2).py

--- Calculadora de Horas  (ver. 0.2).py
+++ Calculadora de Horas  (ver. 0.2).py
@@ -1,17 +1,27 @@
 #fazer uma função para voltar no menu/ fazer uma nova operação ou não...
 a=0
+print("*** calculadora de horas ver.0.2 (alpha)***")
 while (a==0):
- print("*** calculadora de horas ver.0.2 (alpha)***")
- opcao=int(input("\nDigite a opção:\n1 - Converter Horas para Minutos\n2 - Converter Horas para Segundos"
+ opcao=int(input("Digite a opção:\n1 - Converter Horas para Minutos\n2 - Converter Horas para Segundos"
                  "\nDigite 0 para fechar: "))
  #0 - Fechar o Programa
  if(opcao==0):
   a=a+1
- #1 - Horas Para Minuto
+ #1 - Horas para Minutos
  if (opcao==1):
-   hora = int(input("\n*** Convertendo Hora para Minuto***\n   Digite o valor para hora: "))
-   resultado = hora * 60
-   if (resultado == 1) or (hora == 1):
-     print("\n*** Resultado ***\n{} hora é {} minutos\n".format(hora, resultado))
+   hora=int(input("\n*** Convertendo Hora para Minuto ***\n   Digite o valor para hora: "))
+   resultado=hora*60
+   #Colocando a frase no singular
+   if (hora==1):
+     print("\n*** Resultado ***\n{} hora são {} minutos\n".format(hora, resultado))
+   #Colocando a frase no plural
    else:
      print("\n{} horas são {} minutos.\n".format(hora, resultado))
+ #2 - Horas para Segundos
+ if (opcao==2):
+  hora=int(input("\nDigite o valor para hora: "))
+  resultado=hora*3600
+  if (resultado == 1):
+   print("\n*** Resultado ***\n{} minutos é {} hora\n".format(hora, resultado))
+  else:
+   print("\n{} horas são {} segundos\n".format(hora, resultado))
