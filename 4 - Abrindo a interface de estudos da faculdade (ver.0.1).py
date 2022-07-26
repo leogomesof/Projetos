165ad2af2b04016c12e3afd26a25b8efaac6b26f
Leonardo Gomes <90984113+leogomesof@users.noreply.github.com>
2022-07-26 15:35:42 -0300

    Update 4 - Abrindo a interface de estudos da faculdade (ver.0.1).py

--- 4 - Abrindo a interface de estudos da faculdade (ver.0.1).py
+++ 4 - Abrindo a interface de estudos da faculdade (ver.0.1).py
@@ -1,6 +1,10 @@
-#esses valores de pixeis no .click() são de acordo com a minha resolução (1920x1080)
+#Esses valores de pixeis no .click() são de acordo com a minha resolução (1920x1080)
+#Para ajustar a resolução, recomendo rodar um time.sleep(4) print(pyautogui.position()) para descobrir a posição do mouse e colocar no .click()
+
+#importando bibliotecas
 import pyautogui
 import time
+
 #abrindo navegador
 pyautogui.alert(text='Iniciando o programa', title='Nead Opener 2.0', button='OK')
 pyautogui.hotkey("winleft", "d")
@@ -8,6 +12,7 @@ pyautogui.write("c")
 pyautogui.press("enter")
 time.sleep(1)
 pyautogui.hotkey("winleft", "up")
+
 #acessando o site
 pyautogui.write("http://www2.ugb.edu.br/")
 pyautogui.press("enter")
@@ -15,6 +20,7 @@ time.sleep(2)
 pyautogui.click(715,107)
 time.sleep(1)
 pyautogui.click(745,505)
+
 #colocando a matrícula e a senha
 a=pyautogui.password(text='Digite a sua matrícula: ', title='Nead Opener 2.0')
 pyautogui.write(a)
@@ -22,6 +28,7 @@ pyautogui.click(723,567)
 b=pyautogui.password(text='Digite a sua senha: ', title='Nead Opener 2.0', mask='*')
 pyautogui.write(b)
 pyautogui.press("enter")
+
 #acessando o NEAD
 time.sleep(2)
 pyautogui.click(146,799)
